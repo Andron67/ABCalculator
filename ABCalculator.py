1ce0cd69894b97003a4e70c33cf7f3b39fa6523e
Andron67 <zizin.a@mail.ru>
2023-09-01 14:56:04 +0200

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,12 @@
+# A/B Калькулятор
+
+import tkinter as tk
+
+# Создание главного окна
+
+root = tk.Tk()
+root.geometry("280x280")
+root.title("ABCalculator")
+
+# Запуск цикла mainloop
+root.mainloop()
